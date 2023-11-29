4a7ea5dacb8af18d2f0d368cc84a034a739e899d
Jeevan Dulla <jeevannani549@gmail.com>
2023-11-29 10:13:53 -0600

    First commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -167,8 +167,8 @@ export default function Game() {
 
 function calculateWinner(squares) {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
+    [0, 1, 3],
+    [2, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
